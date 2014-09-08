48f72689d9bd045be31f6e8a66d8ca1a900dba4f
polygonplanet <polygon.planet.aqua@gmail.com>
2014-09-08 19:24:20 +0900

    Add 403

---
+++ app/views/errors/403.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="<?php echo $this->getCharset() ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>403 - Forbidden</title>
+<style>
+body {
+  font-family: tahoma, sans-serif;
+  margin: 1.5em;
+}
+</style>
+</head>
+<body>
+  <h1>403 - Forbidden</h1>
+  <p>
+    You don't have permission to access this directory.
+  </p>
+</body>
+</html>
