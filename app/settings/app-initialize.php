f94212be0608b4d70c794bc374d9b5050b04328f
polygonplanet <polygon.planet.aqua@gmail.com>
2014-09-13 18:56:51 +0900

    Add example event

--- app/settings/app-initialize.php
+++ app/settings/app-initialize.php
@@ -14,7 +14,16 @@
  * $this->event->on('app-after-render', function ($app, $template) {});
  * $this->event->on('app-before-render-error', function ($app, $code) {});
  * $this->event->on('app-after-render-error', function ($app, $code) {});
- * $this->event->on('app-before-filter', function ($app, $param1 = null, $param2 = null) {});
- * $this->event->on('app-after-filter', function ($app, $param1 = null, $param2 = null) {});
+ * $this->event->on('app-before-filter', function ($app, $params = array()) {});
+ * $this->event->on('app-after-filter', function ($app, $params = array()) {});
  */
 
+/**
+ * // Handle the database error
+ * $this->event->on('app-database-error', function ($app, $error) {
+ *   if ($app->getDebug()) {
+ *     // Show error message only in debug mode
+ *     echo $app->escapeHTML($error);
+ *   }
+ * });
+ */
